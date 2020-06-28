7e491613f3fba904721b2c01472cbd63b9a7ec2c
Raj Kumar Sanpui <raj.kumar.sanpui@gmail.com>
2020-06-29 01:10:35 +0530

    Add files via upload

---
+++ Box2D-touch-events/Classes/HelloWorldScene.h
@@ -0,0 +1,52 @@
+
+
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+#include "Box2D/Box2D.h"
+#include "GLES-Render.h"
+#include "MyContactListener.h"
+
+enum class PhysicsCategory {
+    None = 0,
+    Bomba = (1 << 0),    // 1
+    Box = (1 << 1), // 2
+    All = PhysicsCategory::Bomba | PhysicsCategory::Box // 3
+};
+
+class HelloWorld : public cocos2d::Scene
+//class HelloWorld : public cocos2d::CCLayer
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+    b2World* m_world;
+
+    void tick(float dt);
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+
+    //virtual void draw(cocos2d::Renderer* renderer, const cocos2d::Mat4& transform, uint32_t flags) override;
+    cocos2d::Mat4 _modelViewMV;
+    //void onDraw();
+    cocos2d::CustomCommand _customCommand;
+    //GLESDebugDraw m_debugDraw;
+    b2Body* groundBody;
+    b2Body* dynamicBody1, *dynamicBody2;
+    cocos2d::Sprite *ball, *ball1;
+    // implement the "static create()" method manually
+    void initTouch();
+
+    void ccTouchesMoved(cocos2d::Touch* touch, cocos2d::Event* evento);
+
+
+    MyContactListener *contactListener;
+
+    CREATE_FUNC(HelloWorld);
+
+    //GLESDebugDraw m_debugDraw;
+};
+
+#endif // __HELLOWORLD_SCENE_H__
